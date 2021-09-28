9e6debeb7944bcf7548f566a58c6bf3e1a9bc653
Viet Anh Tran <tranvietanh1996@gmail.com>
2021-09-29 12:35:19 +1300

    Refactor

--- src/nextBigger.js
+++ src/nextBigger.js
@@ -4,23 +4,9 @@ export const nextBigger = (number) => {
   }
   const digits = number.toString().split("");
   for (let index = digits.length - 1; index > -1; index--) {
-    const currentDigit = digits[index];
-    const digitsToTheRight = digits.slice(index).sort((a, b) => a - b);
-    const nextBiggerDigit = digitsToTheRight.find(
-      (digit) => digit > currentDigit
-    );
-    if (nextBiggerDigit) {
-      const nextBiggerDigitIndex = digitsToTheRight.indexOf(nextBiggerDigit);
-      const digitsToTheRightWithoutBiggerDigit = [
-        ...digitsToTheRight.slice(0, nextBiggerDigitIndex),
-        ...digitsToTheRight.slice(nextBiggerDigitIndex + 1),
-      ];
-      const nextBiggerNumber = [
-        ...digits.slice(0, index),
-        nextBiggerDigit,
-        ...digitsToTheRightWithoutBiggerDigit,
-      ].join("");
-      return parseInt(nextBiggerNumber);
+    const nextBiggerNumber = findNextBiggerNumber(digits, index);
+    if (nextBiggerNumber) {
+      return nextBiggerNumber;
     }
   }
   return -1;
@@ -30,4 +16,43 @@ const areAllDigitSame = (number) => {
   const digits = number.toString().split("");
   const differentDigits = digits.filter((digit) => digit !== digits[0]);
   return !differentDigits.length;
+};
+
+const findNextBiggerNumber = (digits, currentDigitIndex) => {
+  const currentDigit = digits[currentDigitIndex];
+  const digitsToTheRight = sortDigitsToTheRight(digits, currentDigitIndex);
+  const biggerDigitIndex = digitsToTheRight.findIndex(
+    (digit) => digit > currentDigit
+  );
+  const hasFoundBiggerDigit = biggerDigitIndex > -1;
+  if (hasFoundBiggerDigit) {
+    return createBiggerNumber(
+      currentDigitIndex,
+      biggerDigitIndex,
+      digits,
+      digitsToTheRight
+    );
+  }
+};
+
+const sortDigitsToTheRight = (digits, currentDigitIndex) => {
+  return digits.slice(currentDigitIndex).sort((a, b) => a - b);
+};
+
+const createBiggerNumber = (
+  currentDigitIndex,
+  biggerDigitIndex,
+  digits,
+  digitsToTheRight
+) => {
+  const digitsToTheRightWithoutBiggerDigit = [
+    ...digitsToTheRight.slice(0, biggerDigitIndex),
+    ...digitsToTheRight.slice(biggerDigitIndex + 1),
+  ];
+  const nextBiggerNumber = [
+    ...digits.slice(0, currentDigitIndex),
+    digitsToTheRight[biggerDigitIndex],
+    ...digitsToTheRightWithoutBiggerDigit,
+  ].join("");
+  return parseInt(nextBiggerNumber);
 };
