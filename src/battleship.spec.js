0fc4df0ac0f574c57df2276ca1e1924d66e9b7c8
Viet Anh Tran <tranvietanh1996@gmail.com>
2021-09-28 18:37:59 +1300

    Add prettier

--- src/battleship.spec.js
+++ src/battleship.spec.js
@@ -1,7 +1,20 @@
-import { add } from "./battleship";
+import { validateBattlefield } from "./battleship";
 
 describe("Battleship", () => {
-    it('should add', () => {
-        expect(add(1,2)).toEqual(3);
-    })
-})
+  it("should validateBattlefield", () => {
+    expect(
+      validateBattlefield([
+        [1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+        [1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+        [1, 0, 1, 0, 1, 1, 1, 0, 1, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      ])
+    ).toBe(true);
+  });
+});
