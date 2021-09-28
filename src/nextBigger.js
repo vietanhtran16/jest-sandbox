cf1728587d85895523ed559827177b86f7d684ea
Viet Anh Tran <tranvietanh1996@gmail.com>
2021-09-28 22:26:49 +1300

    Handle most scenario

--- src/nextBigger.js
+++ src/nextBigger.js
@@ -2,11 +2,19 @@ export const nextBigger = (number) => {
   if (areAllDigitSame(number)) {
     return -1;
   }
-  return 10;
+  const digits = number.toString().split("");
+  const lastDigit = digits[digits.length - 1];
+  const secondToLastDigit = digits[digits.length - 2];
+  const nextBiggerNumber = [
+    ...digits.slice(0, digits.length - 2),
+    lastDigit,
+    secondToLastDigit,
+  ].join("");
+  return parseInt(nextBiggerNumber);
 };
 
 const areAllDigitSame = (number) => {
   const digits = number.toString().split("");
-  const sum = digits.reduce((total, digit) => total + parseInt(digit), 0);
-  return sum % digits.length === 0;
+  const differentDigits = digits.filter((digit) => digit !== digits[0]);
+  return !differentDigits.length;
 };
